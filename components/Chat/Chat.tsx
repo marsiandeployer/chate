0aea5a783fdcd57496b15f3d19cdf251e0f51f9e
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-04-07 17:48:03 +0000

    replace chat welcome screen

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -157,8 +157,8 @@ export const Chat: FC<Props> = memo(
         {!(apiKey || serverSideApiKeyIsSet) ? (
           <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
             <div className="text-center text-4xl font-bold text-black dark:text-white">
-              Welcome to Chatbot UI
-            </div>
+        привет
+      </div>
             <div className="text-center text-lg text-black dark:text-white">
               <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
               <div className="mb-2 font-bold">
