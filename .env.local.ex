df662daf384fdce38743de2bff1af023ecc10c49
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-04-10 13:39:09 +0000

    replace chat welcome screen

---
+++ .env.local.ex
@@ -0,0 +1,6 @@
+# Chatbot UI
+DEFAULT_MODEL="gpt-3.5-turbo"
+DEFAULT_SYSTEM_PROMPT="You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown."
+OPENAI_API_KEY="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"
+
+
